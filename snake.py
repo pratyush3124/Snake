b351f2c2913984178ba5cac2e511048c6e99bd09
pratyush3124 <connect2pratyush@gmail.com>
2020-03-28 13:08:06 +0530

    one tail added

--- snake.py
+++ snake.py
@@ -17,73 +17,75 @@ class Grid(Canvas):
 
         Canvas.__init__(self, parent, width = 300, height = 300)
 
-        # self.a = randint(0,9)
-        # self.b = randint(0,9)
+        # self.head[0] = randint(0,9)
+        # self.head[1] = randint(0,9)
 
-        self.a = 4
-        self.b = 4
+        self.head = [4,4, 'yellow']
+
+        self.tail = [3,4, 'orange']
 
         for i in range(15):
             for j in range(15):
                 self.create_rectangle(i*20, j*20, 20*(i+1), 20*(j+1), fill = 'gray')
 
-        self.create_rectangle(self.a*20, self.b*20, 20*(self.a+1), 20*(self.b+1), fill = 'yellow')
+        self.create_rectangle(self.head[0]*20, self.head[1]*20, 20*(self.head[0]+1), 20*(self.head[1]+1), fill = self.head[2])
+        self.create_rectangle(self.tail[0]*20, self.tail[1]*20, 20*(self.tail[0]+1), 20*(self.tail[1]+1), fill = 'orange')
 
-                
         self.focus_set()
         self.bind('<Key>', self.changemove)
 
-        self.direction = 'a'
+        self.direction = 's'
 
         self.keepmoving()
 
+    def movehead(self, dir):
 
-
-    def move(self, dir):
+        self.movetail(self.head)
 
         if dir == 'w':
-            self.create_rectangle(self.a*20, self.b*20, 20*(self.a+1), 20*(self.b+1), fill = 'gray')
-            self.b -= 1
-            self.outside()
-            self.create_rectangle(self.a*20, self.b*20, 20*(self.a+1), 20*(self.b+1), fill = 'yellow')
+            self.head[1] -= 1
+            self.ifoutside()
+            self.create_rectangle(self.head[0]*20, self.head[1]*20, 20*(self.head[0]+1), 20*(self.head[1]+1), fill = self.head[2])
 
         if dir == 'a':
-            self.create_rectangle(self.a*20, self.b*20, 20*(self.a+1), 20*(self.b+1), fill = 'gray')
-            self.a -= 1
-            self.outside()
-            self.create_rectangle(self.a*20, self.b*20, 20*(self.a+1), 20*(self.b+1), fill = 'yellow')
+            self.head[0] -= 1
+            self.ifoutside()
+            self.create_rectangle(self.head[0]*20, self.head[1]*20, 20*(self.head[0]+1), 20*(self.head[1]+1), fill = self.head[2])
 
         if dir == 's':
-            self.create_rectangle(self.a*20, self.b*20, 20*(self.a+1), 20*(self.b+1), fill = 'gray')
-            self.b += 1
-            self.outside()
-            self.create_rectangle(self.a*20, self.b*20, 20*(self.a+1), 20*(self.b+1), fill = 'yellow')
+            self.head[1] += 1
+            self.ifoutside()
+            self.create_rectangle(self.head[0]*20, self.head[1]*20, 20*(self.head[0]+1), 20*(self.head[1]+1), fill = self.head[2])
 
         if dir == 'd':
-            self.create_rectangle(self.a*20, self.b*20, 20*(self.a+1), 20*(self.b+1), fill = 'gray')
-            self.a += 1
-            self.outside()
-            self.create_rectangle(self.a*20, self.b*20, 20*(self.a+1), 20*(self.b+1), fill = 'yellow')
+            self.head[0] += 1
+            self.ifoutside()
+            self.create_rectangle(self.head[0]*20, self.head[1]*20, 20*(self.head[0]+1), 20*(self.head[1]+1), fill = self.head[2])
+
+    def movetail(self, behind):
+        self.create_rectangle(self.tail[0]*20, self.tail[1]*20, 20*(self.tail[0]+1), 20*(self.tail[1]+1), fill = 'gray')
+        self.create_rectangle(behind[0]*20, behind[1]*20, 20*(behind[0]+1), 20*(behind[1]+1), fill = self.tail[2])
+        self.tail[0], self.tail[1] = self.head[0], self.head[1]
 
     def changemove(self, event):
         self.direction = event.char
 
     def keepmoving(self):
-        self.move(self.direction)
+        self.movehead(self.direction)
         self.parent.after(300, self.keepmoving)
 
-    def outside(self):
-        if self.a not in range(15):
-            if self.a > 5:
-                self.a -=15
-            elif self.a < 5:
-                self.a +=15
+    def ifoutside(self):
+        if self.head[0] not in range(15):
+            if self.head[0] > 5:
+                self.head[0] -=15
+            elif self.head[0] < 5:
+                self.head[0] +=15
         
-        if self.b not in range(15):
-            if self.b > 5:
-                self.b -=15
-            elif self.b < 5:
-                self.b +=15
+        if self.head[1] not in range(15):
+            if self.head[1] > 5:
+                self.head[1] -=15
+            elif self.head[1] < 5:
+                self.head[1] +=15
 
 app = Snake()
 app.mainloop()
